4172ec4074a4019d84cde5c61e76fa1fcbb966b8
mokhs00 <mokhs00@naver.com>
2021-01-03 09:22:29 +0900

    update : 전화번호_목록.java
    1. break는 for문 하나만 빠져나감. -> 경우에 따라 더 빠른 알고리즘을 위해서 return으로 변경함.
    2. String.startsWith() 사용 방식 추가

--- src/main/java/Hash/전화번호_목록.java
+++ src/main/java/Hash/전화번호_목록.java
@@ -1,6 +1,5 @@
 package Hash;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class 전화번호_목록 {
@@ -9,36 +8,43 @@ public class 전화번호_목록 {
     public static void main(String[] args) {
         String[] phone_book = {"119", "97674223", "1195524421"};
 
-        System.out.println(phone_book[0].length());
-//        System.out.println(solution(phone_book));
-
+//        System.out.println(phone_book[0].length());
+        long start = System.currentTimeMillis();
+        System.out.println(solution(phone_book));
+        System.out.print("Millis : ");
+        long end = System.currentTimeMillis();
+        System.out.println((end - start));
     }
 
     private static boolean solution(String[] phone_book) {
+//        for (int i = 0; i < phone_book.length - 1; i++) {
+//            for (int j = i + 1; j < phone_book.length; j++) {
+//                if (phone_book[i].startsWith(phone_book[j])) {
+//                    return false;
+//                }
+//                if (phone_book[j].startsWith(phone_book[i])) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+
         boolean answer = true;
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            for (int j = i + 1; j < phone_book.length; j++) {
 
+                String s = phone_book[i];
+                String target = phone_book[j];
 
-        HashSet<String> hashSet = new HashSet();
-        for (String s : phone_book) {
-            hashSet.add(s);
-        }
-
-        for (String s : hashSet) {
-            for (String target : hashSet) {
-                if (s.equals(target))
-                    continue;
                 if (target.length() >= s.length() && target.substring(0, s.length()).equals(s)) {
-                    answer = false;
-                    break;
+                    return false;
+
                 }
-                if (target.length() < s.length() && s.substring(0, target.length()).equals(target)){
-                    answer = false;
-                    break;
+                if (target.length() < s.length() && s.substring(0, target.length()).equals(target)) {
+                    return false;
                 }
             }
         }
-
-
         return answer;
     }
 
