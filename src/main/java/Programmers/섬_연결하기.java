ea3742adff3ae1d1dab58ad15d1b2fa43c48de5d
mokhs00 <mokhs00@naver.com>
2021-04-22 21:45:20 +0900

    update : Programmers 섬_연결하기 문제 링크 추가 및 복습

--- src/main/java/Programmers/섬_연결하기.java
+++ src/main/java/Programmers/섬_연결하기.java
@@ -3,17 +3,22 @@ package Programmers;
 import java.util.Arrays;
 import java.util.Comparator;
 
+// https://programmers.co.kr/learn/courses/30/lessons/49191?language=java
 public class 섬_연결하기 {
+    /*
+        최소 비용으로 모든 노드를 연결해 최소 비용 트리를 만드는 크루스칼 알고리즘
+     */
     int[] parents;
 
     public int solution(int n, int[][] costs) {
         int answer = 0;
-
         parents = new int[n];
+
         for (int i = 0; i < parents.length; i++) {
             parents[i] = i;
         }
 
+        // 간선의 비용 순으로 정렬하고
         Arrays.sort(costs, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -21,31 +26,35 @@ public class 섬_연결하기 {
             }
         });
 
+        // a와 b가 연결되어 있지 않다면 연결해주고 비용 추가
+        // a와 b가 연결되어 있다면 더하지 않음
+        // 연결되어 있음은 UnionFind를 이용
+        for (int[] c : costs) {
+            int a = c[0];
+            int b = c[1];
+            int cost = c[2];
 
-        for (int[] cost : costs) {
-            int start = cost[0];
-            int end = cost[1];
-            int c = cost[2]; // cost
-
-
-            int startParent = findParent(start);
-            int endParent = findParent(end);
-            if (startParent != endParent) {
-                if (startParent < endParent)
-                    parents[endParent] = startParent;
-                else
-                    parents[startParent] = endParent;
-                answer += c;
+            int parentA = getParent(a);
+            int parentB = getParent(b);
+            if (parentA != parentB) {
+                answer += cost;
+                if (parentA > parentB) {
+                    parents[parentA] = parentB;
+                } else
+                    parents[parentB] = parentA;
             }
         }
 
         return answer;
     }
 
-    public int findParent(int child) {
-        if (parents[child] == child) {
+    public int getParent(int child) {
+
+        if (child == parents[child]) {
             return child;
         }
-        return findParent(parents[child]);
+        return getParent(parents[child]);
     }
 }
+
+
