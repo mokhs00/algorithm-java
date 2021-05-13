2a139cce909a3950b1ffcf65d4da6812b8eea31e
mokhs00 <mokhs00@naver.com>
2021-05-13 21:22:25 +0900

    repeat solve : Programmers H_Index

--- src/main/java/Programmers/H_Index.java
+++ src/main/java/Programmers/H_Index.java
@@ -2,15 +2,30 @@ package Programmers;
 
 import java.util.Arrays;
 
+// https://programmers.co.kr/learn/courses/30/lessons/42747?language=java
 class H_Index {
+    /*
+        H-Index :
+        과학자가 발표한 논문 n 편 중,
+        h번 이상 인용된 논문이
+        h편 이상이고,
+        나머지 논문이 h번 이하 인용되었을 때.
+        h의 최댓값
+
+        을 구하여라.
+
+        정렬 이용.
+
+     */
     public int solution(int[] citations) {
         int answer = 0;
 
         Arrays.sort(citations);
 
-        for(int i = 0; i < citations.length; i++){
-            int h = citations[i];
-            if(h >= citations.length - i){
+        for (int i = 0; i < citations.length; i++) {
+            int now = citations[i];
+            // citations.length - i = citations.length -1 - i + 1
+            if (now >= citations.length - i) {
                 return citations.length - i;
             }
         }
