a60448038d56d7c6dd95f998039af0225d71fbb6
mokhs00 <mokhs00@naver.com>
2021-07-27 19:01:09 +0900

    refactor : BOJ 오큰수_17298
    알고리즘 정리 후 코드 깔끔하게 변경함.
    성능 차이는 크게 없는듯,,

--- src/main/java/BOJ/오큰수_17298.java
+++ src/main/java/BOJ/오큰수_17298.java
@@ -22,6 +22,23 @@ public class 오큰수_17298 {
           이전 값이 더 큰 경우를 찾으면 해당 값을 sb.append 해주고 stack에 현재 값을 넣음
           못 찾으면 sb.append("-1 ") 해주고 stack에 현재 값을 넣음
 
+
+         refactor
+
+         코드가 너무 난잡해져서 알고리즘을 재정리하고 refactoring 해보자.
+
+         - 수열을 오른쪽에서 왼쪽으로 탐색함
+         - answer 스택에 출력 값을 담는다.
+         - 다음 조건에 따르면서 수열을 탐색
+           1. 현재 값(current) > 이전 값(prev) ->
+                !temp.isEmpty() && prev < current 인 동안(while) stack.pop 함
+                while 문을 빠져나온 이후에 스택이 비어있다면 answer.push(-1)
+
+           2. 현재 값(current) < 이전 값(prev) -> answer.push(prev)
+
+           - 위 조건 이후 temp.push(current)
+         - answer에 담긴 값을 출력 형식에 맞게 sb.append 해서 출력함
+
      */
 
     public static void main(String[] args) throws IOException {
@@ -29,43 +46,23 @@ public class 오큰수_17298 {
         String[] lines = br.readLine().split(" ");
         StringBuilder sb = new StringBuilder();
 
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> temp = new Stack<>();
         Stack<Integer> answer = new Stack<>();
 
-        // init
-        answer.push(-1);
-        stack.push(Integer.parseInt(lines[lines.length - 1]));
-
-        for (int i = lines.length - 2; i >= 0; i--) {
+        for (int i = lines.length -1; i >= 0; i--) {
             int current = Integer.parseInt(lines[i]);
-            Integer right = stack.peek();
 
-            if (current < right) {
-                answer.push(right);
-                stack.push(current);
-                continue;
+            while (!temp.isEmpty() && current >= temp.peek()) {
+                temp.pop();
             }
 
-            stack.pop();
-
-            while (!stack.isEmpty()) {
-                Integer peek = stack.peek();
-                if (current >= peek) {
-                    stack.pop();
-                } else {
-                    answer.push(peek);
-                    break;
-                }
-            }
-
-            if (stack.isEmpty()) {
+            if (temp.isEmpty()) {
                 answer.push(-1);
-                stack.push(current);
-                continue;
+            }else if (current < temp.peek()) {
+                answer.add(temp.peek());
             }
 
-            stack.push(current);
-
+            temp.push(current);
         }
 
         while (!answer.isEmpty()) {
@@ -78,6 +75,5 @@ public class 오큰수_17298 {
         br.close();
         bw.close();
     }
-
 
 }
